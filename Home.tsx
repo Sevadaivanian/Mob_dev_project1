fefaaa25c2504ea5aa9187d27c6ae81ea391274a
Sevada <sevadaivanian@gmail.com>
2024-04-04 11:56:48 +0200

    4 commit

--- Home.tsx
+++ Home.tsx
@@ -1,29 +1,78 @@
 import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Image } from "expo-image";
 
 const Home = () => {
+
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const [BMI, setBMI] = useState(0.0);
+
+  
+
+  const handlerCalcBMI = () => {
+    const currentWeight = parseInt(weight);
+    const currentHeight = parseInt(height);
+
+    if (isNaN(currentWeight) || isNaN(currentHeight)) {
+      return;
+    }
+
+    const heightMetrs = currentHeight / 100;
+    setBMI(Number((currentWeight / (heightMetrs * heightMetrs)).toFixed(1)));
+  };
+
   return <View style={styles.container}>
     <Text style={styles.titleText}>BMI Calculator</Text>
     <View style={styles.inputContainer}>
     <TextInput 
+    value={height}
+    onChangeText={(value) => {
+      setHeight(value);
+    }}
     style={styles.input} 
     placeholder="Height-M" 
     placeholderTextColor={'rgba(90,90,190,1)'} 
     keyboardType="number-pad"/>
     <TextInput 
+    value={weight}
+    onChangeText={(value) => {
+      setWeight(value);
+    }}
     style={styles.input}
       placeholder="Weight-M"
       placeholderTextColor={'rgba(90,90,190,1)'}
       keyboardType="number-pad"/>
       </View>
-      <TouchableOpacity style={styles.goButton}>
+      <TouchableOpacity style={styles.goButton}
+      onPress={() => {
+        handlerCalcBMI();
+      }}>
         <Text style={styles.goButtonText}>Go</Text>
         </TouchableOpacity>
-        <Text style={styles.BMIText}>0.00</Text>
+        <Text style={styles.BMIText}>{BMI}</Text>
+        <View style={styles.line}></View>
         <View style={styles.RectContainer}>
-          <View style={styles.RectYellow}></View>
-          <View style={styles.RectGreen}></View>
-          <View style={styles.RectRed}></View>
+          <View style={[styles.RectYellow,
+            BMI <= 18.5 && BMI >= 1 && { opacity: 1 }]}><Image
+          style={styles.YellowImage}
+          source={require('./assets/yellow.png')}
+          contentFit="fill"
+          />
+          <Text style={styles.valueText}>Under 18.5</Text>
+          <Text style={styles.noteText}>Under Weight</Text>
+          </View>
+          <View style={[styles.RectGreen, BMI > 18.5 && BMI < 25 && { opacity: 1 }]}><Image style={styles.YellowImage}
+          source={require('./assets/green.png')}
+          contentFit="fill" />
+          <Text style={styles.valueText}>18.6-25</Text>
+          <Text style={styles.noteText}>Normal Weight</Text></View>
+          <View style={[styles.RectRed, BMI >= 25 && { opacity: 1 }]}><Image style={styles.YellowImage}
+          source={require('./assets/red.png')}
+          contentFit="fill" />
+          <Text style={styles.valueText}>Above 25</Text>
+          <Text style={styles.noteText}>Over Weight</Text></View>
         </View>
   </View>
 
@@ -72,26 +121,55 @@ const styles = StyleSheet.create({
     fontSize:35,
     marginTop:40
   },
+  line:{
+        width:'100%',
+        height:2,
+        backgroundColor:'white',
+        marginVertical:50,
+        opacity:0.6
+  },
+
   RectContainer:{
     flexDirection:'row',
-    gap:15
+    gap:15,
+    paddingHorizontal:10
   },
   RectYellow:{
     width:100,
     height:200,
     backgroundColor:'yellow',
+    borderRadius:15,
+    opacity:0.3,
+    alignItems:"center",
     
   },
   RectGreen:{
     width:100,
     height:200,
     backgroundColor:'green',
+    borderRadius:15,
+    opacity:0.3,
+    alignItems:"center",
     
   },RectRed:{
     width:100,
     height:200,
     backgroundColor:'red',
+    borderRadius:15,
+    opacity:0.3,
+    alignItems:"center",
+  },
+    YellowImage:{
+      height:70,
+      width:100,
+      marginTop:20
+  },
+  valueText:{
     
+    marginTop:30
+  },
+  noteText:{
+    marginTop:10
   }
 });
 
